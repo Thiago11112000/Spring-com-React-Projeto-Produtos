4374725156881943eb9ecc931ab70d397b254adc
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-04-02 17:10:34 -0300

    configurando controle

--- produtos/src/main/java/br/com/api/produtos/ProdutosApplication.java
+++ produtos/src/main/java/br/com/api/produtos/ProdutosApplication.java
@@ -2,12 +2,12 @@ package br.com.api.produtos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "br.com.api.produtos.repositorio")
 public class ProdutosApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ProdutosApplication.class, args);
     }
-
 }
