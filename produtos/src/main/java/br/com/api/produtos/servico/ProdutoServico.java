27a7fa01ed73d5c3f10625b02828bc5a4a1496a0
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-04-03 08:39:01 -0300

    serviço para listar todos os produtos

--- produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ produtos/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -1,4 +1,18 @@
 package br.com.api.produtos.servico;
 
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.repositorio.ProdutoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProdutoServico {
+
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    // Método para listar todos os produtos
+    public  Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
 }
