5c7a41def15871828461429986403588f62ae722
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2025-04-05 10:55:43 -0300

    Estrutura do formulário

--- front/src/Formulario.jsx
+++ front/src/Formulario.jsx
@@ -1,6 +1,15 @@
 function Formulario(){
     return(
-        <h1>Formulário </h1>
+        <form>
+        <input type= "text" placeholder="Nome"/>
+        <input type= "text" placeholder="Marca"/>
+
+        <input type="button" value = "Cadastrar"/>
+        <input type="button" value = "Alterar"/>
+        <input type="button" value = "Remover"/>
+        <input type="button" value = "Cancelar"/>
+
+        </form>
     )
 }
 export default Formulario;
